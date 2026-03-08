7c542ad25cfeaaf1302d50143ce4521a28512310
wieszheng <wieszheng@users.noreply.github.com>
2026-03-08 22:39:14 +0800

    fix: add TeamPage route to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import RegisterPage from './pages/RegisterPage'
 import DashboardPage from './pages/DashboardPage'
 import TasksPage from './pages/TasksPage'
 import TaskDetailPage from './pages/TaskDetailPage'
+import TeamPage from './pages/TeamPage'
 import ProfilePage from './pages/ProfilePage'
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
             <Route index element={<DashboardPage />} />
             <Route path="tasks" element={<TasksPage />} />
             <Route path="tasks/:taskId" element={<TaskDetailPage />} />
+            <Route path="team" element={<TeamPage />} />
             <Route path="profile" element={<ProfilePage />} />
           </Route>
         </Routes>
